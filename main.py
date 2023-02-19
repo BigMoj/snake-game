4bce08dada93fae9bae971d394da846f781e28ab
BigMoj <mojthegreat@gmail.com>
2023-02-19 18:11:18 +0330

    added food and get random position when snake hit food

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from food import Food
 from snake import Snake
 import time
 
@@ -9,6 +10,14 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
@@ -16,6 +25,8 @@ while game_is_on:
     time.sleep(0.1)
 
     snake.move()
+    if snake.head.distance(food) < 15:
+        food.random_position()
 
 
 
